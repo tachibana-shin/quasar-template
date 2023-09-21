14e195f46cba86c29bc307324effe68b6a1656a2
Tachibana Shin <tachibshin@duck.com>
2023-09-21 16:26:22 +0000

    fix action

--- quasar.config.ts
+++ quasar.config.ts
@@ -51,7 +51,7 @@ export default configure((/* ctx */) => {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ["supabase", "i18n", "head", "unocss"],
+    boot: ["i18n", "head", "unocss"],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
     css: ["app.scss"],
